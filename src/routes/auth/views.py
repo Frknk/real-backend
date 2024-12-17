096886d71b6b49328ee2fdd2531448c08aaf8ced
Frknk <frknk1@protonmail.com>
2024-12-16 23:33:25 -0500

    env example

--- src/routes/auth/views.py
+++ src/routes/auth/views.py
@@ -27,7 +27,7 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), ses
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     access_token = create_access_token(data={"sub": db_user.username, "role": db_user.role}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
