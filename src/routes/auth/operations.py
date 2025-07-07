03042a72d0c37d1fd50eb3b091f44aa12d986e03
Frknk <frknk1@protonmail.com>
2025-07-07 11:09:21 -0500

    fix import

--- src/routes/auth/operations.py
+++ src/routes/auth/operations.py
@@ -54,7 +54,7 @@ def create_access_token(data: dict, expires_delta: datetime.timedelta | None = N
     else:
         expire = datetime.datetime.now(
             datetime.timezone.utc
-        ) + datetime.datetime.timedelta(minutes=15)
+        ) + datetime.timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
